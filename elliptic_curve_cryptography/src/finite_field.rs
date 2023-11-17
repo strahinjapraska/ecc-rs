989a38efa3aa7eb8f9546d455ef60ed4919ec202
Strahinja <strahinja.praska6@gmail.com>
2023-11-17 01:28:16 +0100

    Finite field changes

--- elliptic_curve_cryptography/src/finite_field.rs
+++ elliptic_curve_cryptography/src/finite_field.rs
@@ -9,16 +9,23 @@ pub struct FiniteField{
 impl FiniteField{
 
     pub fn add(a: &BigUint, b: &BigUint, p: &BigUint) -> BigUint{
-        (a + b) % p
+        assert!(a < p, "a > p"); 
+        assert!(b < p, "b > p"); 
+
+        (a + b).modpow(&BigUint::from(1u32), &p)
     }
 
     pub fn mul(a: &BigUint, b: &BigUint, p: &BigUint) -> BigUint{
-        (a * b) % p 
+        assert!(a < p, "a > p"); 
+        assert!(b < p, "b > p"); 
+
+        (a * b).modpow(&BigUint::from(1u32), &p)
     }
 
     pub fn inv_add(a: &BigUint, p: &BigUint) -> BigUint{
         // r = - a (mod p)
-        assert!(a < p, "a >= p"); 
+        assert!(a < p, "a > p"); 
+
         p - a
     }
 
@@ -27,11 +34,16 @@ impl FiniteField{
         // we will use Fermat's little theorem, p must be prime  
         // a^(p-1) = 1 (mod p)
         // a^(p-2) = a^-1 (mod p)
+        assert!(a < p, "a > p"); 
+
         a.modpow(&(p-BigUint::from(2u32)),p)
 
     }
     pub fn sub(a: &BigUint, b: &BigUint, p: &BigUint) -> BigUint {
         // r = a-b (mod p) = a + (-b) (mod p)
+        assert!(a < p, "a > p"); 
+        assert!(b < p, "b > p"); 
+
         let b_inv = FiniteField::inv_add(b,p);
         FiniteField::add(a,&b_inv,p)
 
@@ -39,6 +51,9 @@ impl FiniteField{
 
     pub fn div(a: &BigUint, b: &BigUint, p: &BigUint) -> BigUint{
         // r =  a/b (mod p) = a*b^(-1) (mod p)
+        assert!(a < p, "a > p"); 
+        assert!(b < p, "b > p"); 
+        
         let b_inv = FiniteField::inv_mul(b,p); 
         FiniteField::mul(a,&b_inv,p)
     }
